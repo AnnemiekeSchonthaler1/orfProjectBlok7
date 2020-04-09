0a850137ae41b1564b2b798f6421f7736b537eb9
C posthuma <christiaanposthuma@gmail.com>
2020-04-09 21:43:49 +0200

    added some comments

--- BlastResultRepository.java
+++ BlastResultRepository.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class BlastResultRepository {
 
     public static void main(String args[]) {
+        //save the data,  code template
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
--- OrfFinder.java
+++ OrfFinder.java
@@ -171,6 +171,7 @@ public class OrfFinder extends JFrame implements ActionListener {
     public void blastOrfs() {
         try {
             boolean safe = false;
+            ArrayList<String> tempSaveBlastData = new ArrayList();
             orfsArray = makeArrayOfOrfs();
             JLabel name = new JLabel(orfsArray[0]);
             // longValue bepaalt hoeveel zichtbaar is / aantal tekens. Dus heb een lange tab staan voor nu.
@@ -211,6 +212,8 @@ public class OrfFinder extends JFrame implements ActionListener {
                     String line;
                     while ((line = input.readLine()) != null) {
                         System.out.println(line);
+                        tempSaveBlastData.add(line);
+
                     }
                     System.out.println("Ik ben klaar met het uitvoeren van de BLAST.");
                 } catch (IOException e) {
@@ -242,7 +245,7 @@ public class OrfFinder extends JFrame implements ActionListener {
 
             if (safe) {
                 // Hier moeten de resultaten meegegeven
-                safeBlast();
+                safeBlast(sequentieOrf,tempSaveBlastData);
             }
         } catch (NullPointerException | ArrayIndexOutOfBoundsException | IOException e) {
             // do nothing ; wordt in makeArrayOfOrfs al met een messagebox gewaarschuwd. Kreeg het niet voor elkaar
@@ -250,23 +253,36 @@ public class OrfFinder extends JFrame implements ActionListener {
         }
     }
 
-    private void safeBlast() {
+    private void safeBlast(String ORFSequence,ArrayList tempSaveBlastData) {
+        // functioni to save the data to the database, makes use of 3 queries to do so
         try {
             Class.forName("com.mysql.jdbc.Driver");
             Connection con = DriverManager.getConnection(
+                    //connection requirements for the connection with the database
+
                     "jdbc:mysql://hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com:3306/rucia?serverTimezone=UTC",
                     "rucia@hannl-hlo-bioinformatica-mysqlsrv",
                     "kip");
-//here sonoo is database name, root is username and password
-            //for(blast in blastList){
-            /*
+/*
 insert into sequence(  {seq_id } , {seq_varchar } )
 insert into ORF( {ORF_id },{ Sequence_ORF},{Sequence_sequence_id})
 insert into Blast_res ( { description}, {coverage},{e_value}, {loc_start},{Loc_end},{blast_id},{ORF_ORF_id})
  */
-            Statement stmt = con.prepareStatement("insert into blast_res (column1, column2) values (?, ?)");
-            stmt.execute(//blast.id, blast.name);
-                    con.close();
+            String FullSequence = sequenceObj.getSequence();
+            // String ORFSequence
+
+
+            Statement stmt = con.prepareStatement("insert into sequence (Sequence_id, Sequence) values ()");// in values : auto_increment, FullSequence
+            Statement stmt2 = con.prepareStatement("insert into ORF (ORF_id, Sequence_ORF, Sequence_Sequence_id) values ()");// in values : auto_increment,sequentieOrf, sequence ( fullseq id)
+            stmt.execute(//excuteSequence);
+                    stmt2.execute(//excuteORF);
+            int i;
+            for(i=0;i<5;i=0) {
+                Statement stmt3 = con.prepareStatement("insert into blast_res (description, coverage, e_value, location_start, location_end, Blast_id, ORF_ORF_id) values ()");
+                // description, coverage, e_value, locstart, locend, blast ID, ORF_ORF_id ( id ORF)
+                stmt3.execute(//excuteBlast);
+            }
+            con.close();
         } catch (Exception e) {
             System.out.println(e);
         }
